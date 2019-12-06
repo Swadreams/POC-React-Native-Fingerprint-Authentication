f2884e88caea64924e1ded44d351e17919b2c06e
SwapnilC <swadreams@gmail.com>
2019-12-06 15:58:12 +0530

    Biometric Authentication Code

---
+++ src/FingerprintPopup.component.ios.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Alert } from 'react-native';
+import FingerprintScanner from 'react-native-fingerprint-scanner';
+
+class FingerprintPopup extends Component {
+
+  componentDidMount() {
+    FingerprintScanner
+      .authenticate({ description: 'Scan your fingerprint on the device scanner to continue' })
+      .then(() => {
+        this.props.handlePopupDismissed();
+        Alert.alert('Authenticated successfully');
+      })
+      .catch((error) => {
+        this.props.handlePopupDismissed();
+        Alert.alert(error);
+      });
+  }
+
+  render() {
+    return false;
+  }
+}
+
+FingerprintPopup.propTypes = {
+  handlePopupDismissed: PropTypes.func.isRequired,
+};
+
+export default FingerprintPopup;
